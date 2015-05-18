a8aff83d51868c4993898e86188da2f7dd9f5fd3
Aashish Kiran <aashish.kiran@gmail.com>
2015-05-18 18:57:56 +0530

    added custom url without port 1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def custom_url
-    rails.env == 'production' ? request.host : "#{request.host}: #{request.port}"
+    Rails.env == 'production' ? request.host : "#{request.host}: #{request.port}"
   end
 end
